9dd10ebd5eb0a157218130e23d065d28321d2495
Sunny17082 <sunnyprasad1708@gmail.com>
2023-09-16 19:59:23 +0530

    Added auto search feature

---
+++ assets/js/search.js
@@ -0,0 +1,38 @@
+let searchTerms = [
+    "Apple",
+    "Orange",
+    "Banana",
+    "Watermelon",
+    "Mango"
+];
+
+const resultBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function() {
+    let result = [];
+    let input = inputBox.value;
+    if(input.length) {
+        result = searchTerms.filter((keyword) => {
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+    }
+    display(result);
+
+    if(!result.length) {
+        resultBox.innerHTML = "";
+    }
+}
+
+function display(result) {
+    const content = result.map((list) => {
+        return "<li onclick=selectInput(this)>"+ list + "</li>";
+    });
+
+    resultBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function selectInput(list) {
+    inputBox.value = list.innerHTML;
+    resultBox.innerHTML = "";
+}
